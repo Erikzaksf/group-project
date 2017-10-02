39827af7e01b0d84a8757367d8e74e73c7adf5b2
Mitchell <meechllada@gmail.com>
2017-10-02 13:16:43 -0700

    add migrations for users, subjects, sub catagories and a join table subjects, users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002182523) do
+ActiveRecord::Schema.define(version: 20171002183241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sub_catagories", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subjects", force: :cascade do |t|
     t.string "title"
