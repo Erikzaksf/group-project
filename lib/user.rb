6443cba92316f8f4a41d21321bdb57f450ccfac9
Mitchell <meechllada@gmail.com>
2017-10-03 10:20:52 -0700

    add validations for password, first_name and last_name

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  # validates :first_name, length: { minimum: 2, maximum: 15}
-  # validates :last_name, length: { minimum: 2, maximum: 15}
+  validates :first_name, length: { minimum: 2, maximum: 15}
+  validates :last_name, length: { minimum: 2, maximum: 15}
   validates :password, length: { minimum: 8, maximum: 25}
 
 # validates :email, :presence => true,
