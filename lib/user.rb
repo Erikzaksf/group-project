625f4cc481e4c08b699968d74a46395324f478cf
Mitchell <meechllada@gmail.com>
2017-10-03 10:02:04 -0700

    add test ensures password length maximum of 25 chars

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # validates :first_name, length: { minimum: 2, maximum: 15}
   # validates :last_name, length: { minimum: 2, maximum: 15}
-  validates :password, length: { minimum: 8, maximum: 20}
+  validates :password, length: { minimum: 8, maximum: 25}
 
 # validates :email, :presence => true,
 #              :length => {:minimum => 3, :maximum => 254},
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,4 +6,8 @@ describe(User) do
     user = User.new({:password => "a".*(6)})
     expect(user.save()).to(eq(false))
   end
+  it("ensures the length of password is at most 25 characters") do
+    user = User.new({password: "a".*(26)})
+    expect(user.save()).to(eq(false))
+  end
 end
