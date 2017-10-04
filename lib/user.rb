dfb573b68a8c0c8b48bc3dbaf2bfc867c14c5ff6
erikzaksf <erik.zak.sf@gmail.com>
2017-10-04 11:47:22 -0700

    update database and tables to match build

--- lib/user.rb
+++ lib/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   validates :first_name, length: { minimum: 2, maximum: 15}
   validates :last_name, length: { minimum: 2, maximum: 15}
   validates :password, length: { minimum: 2, maximum: 25}
+  has_one(:profile)
+  has_and_belongs_to_many(:meetups)
 end
